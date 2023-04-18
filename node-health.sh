3cea9a7db37d19f907ac2ca2c37a63e386b709cd
demo <sreejagajelli@yahoo.com>
2023-04-18 16:37:23 -0400

    node health v1

---
+++ node-health.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+######################
+# Author : Sreeja
+#
+# Date : 18/Apr/2023
+#
+# This Script outputs the node health
+#
+# Version : v1
+######################
+
+df -h
+
+free -g 
+
+nproc
+
